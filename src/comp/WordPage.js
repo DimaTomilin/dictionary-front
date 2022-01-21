9b29433f6f7bbf54f476c40a645105a2b1124bae
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2022-01-21 15:37:10 +0200

    Add find word functionality
    Add function of search new word by click

--- src/comp/WordPage.js
+++ src/comp/WordPage.js
@@ -1,17 +1,40 @@
+/* eslint-disable */
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import axios from 'axios';
 import { Button } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { takeWord } from '../helpers/wordTaken';
+import { alert } from '../helpers/alerts';
 
 export default function WordPage() {
   const state = useSelector((state) => state);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const clickHandle = () => {
     navigate('/');
   };
+
+  const findWord = async (e) => {
+    if (e.ctrlKey === true) {
+      const word = takeWord();
+      let url = `http://localhost:3000/${word}/`;
+      const { data } = await axios.get(url);
+      if (data.length === 0) {
+        alert('Sorry but we don`t find this word');
+        return;
+      }
+      dispatch({
+        type: 'SET_NEW_WORD',
+        payload: data,
+      });
+      navigate(`/${data[0].Word}`);
+    }
+  };
+
   return (
     <div className="App">
       <Header />
@@ -21,11 +44,11 @@ export default function WordPage() {
         return (
           <div key={word.Definition.slice(0, 5)}>
             <p>{word.Part_of_speech}</p>
-            <p>{word.Definition}</p>
+            <p onClick={testclick}>{word.Definition}</p>
           </div>
         );
       })}
-      <Button variant="primary" className="form-btn" onClick={clickHandle}>
+      <Button variant="primary" className="form-btn" onClick={findWord}>
         To main page
       </Button>
       <hr />
---
+++ src/helpers/wordTaken.js
@@ -0,0 +1,25 @@
+const takeWord = () => {
+  const s = window.getSelection();
+  const range = s.getRangeAt(0);
+  const node = s.anchorNode;
+
+  // Find starting point
+  while (range.toString().indexOf(' ') != 0) {
+    range.setStart(node, range.startOffset - 1);
+  }
+  range.setStart(node, range.startOffset + 1);
+
+  // Find ending point
+  do {
+    range.setEnd(node, range.endOffset + 1);
+  } while (
+    range.toString().indexOf(' ') == -1 &&
+    range.toString().trim() != ''
+  );
+
+  return range
+    .toString()
+    .replace(/[^a-zA-Z0-9]/g, '')
+    .trim();
+};
+module.exports = { takeWord };

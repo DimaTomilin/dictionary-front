9ec6f20f63c04d7fb3ab98d5d6e474d971e21a42
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2022-07-19 09:46:25 +0300

    Fixed bug in function Take Word
    -Bug in clicking on first word in the paragraph
    - Bug in clicking on the words without space between neighbors words

--- src/helpers/wordTaken.js
+++ src/helpers/wordTaken.js
@@ -5,16 +5,29 @@ const takeWord = () => {
   const node = s.anchorNode;
 
   // Find starting point
-  while (range.toString().indexOf(' ') != 0) {
+  while (
+    range.toString().indexOf(' ') != 0 &&
+    range.toString().indexOf(',') != 0 &&
+    range.toString().indexOf('.') != 0
+  ) {
     range.setStart(node, range.startOffset - 1);
+    if (node.data.indexOf(range.toString()[0]) === 0) {
+      break;
+    }
   }
-  range.setStart(node, range.startOffset + 1);
+
+  //Check if first letter of the string it is first letter of the paragraf
+  if (node.data.indexOf(range.toString()[0]) !== 0) {
+    range.setStart(node, range.startOffset + 1);
+  }
 
   // Find ending point
   do {
     range.setEnd(node, range.endOffset + 1);
   } while (
     range.toString().indexOf(' ') == -1 &&
+    range.toString().indexOf(',') == -1 &&
+    range.toString().indexOf('.') == -1 &&
     range.toString().trim() != ''
   );
 

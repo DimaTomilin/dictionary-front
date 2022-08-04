d1b269a886c68658bb9ce48db7c21e1d0f84a687
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2022-08-04 23:25:22 +0300

    Refactoring WordPage
    -Changing Main logic of application
    -Refactoring to ts
    -Adding spinner
    -Improving error message

---
+++ src/pages/WordPage.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable */
+import React, { useContext } from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { useParams, useLocation, useSearchParams } from 'react-router-dom';
+import { ThemeContext } from '../context/ThemeContext';
+import { createFetchURL } from '../helpers/createFetchURL';
+import { useAction } from 'hooks/useAction';
+import ListOfWords from 'components/ListOfWords';
+
+const WordPage: React.FC = () => {
+  const { fetchWords } = useAction();
+  const state = useContext(ThemeContext);
+
+  const params = useParams();
+  const location = useLocation();
+  const [searchParams] = useSearchParams();
+  const letterQueryParams = searchParams.get('letter');
+
+  // @ts-expect-error
+  fetchWords(createFetchURL(params, location.pathname, letterQueryParams));
+
+  return (
+    <div className={`App ${state?.darkMode ? 'darkmode' : 'lightmode'}`}>
+      <Header />
+      <hr />
+      <ListOfWords />
+      <hr />
+      <Footer />
+    </div>
+  );
+};
+
+export default WordPage;

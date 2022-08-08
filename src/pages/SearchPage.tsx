91521d706b26a94981f0178993c200e8afe510eb
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2022-08-08 16:43:20 +0300

    Alert Message
    Improving alert message in SearchPage
    Now user will see it only if he browse from computer or tablet

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -4,14 +4,19 @@ import Main from '../components/Main';
 import Footer from '../components/Footer';
 import AlertMessage from '../components/ui/AlertMessage';
 import { ThemeContext } from '../context/ThemeContext';
+import { UAParser } from 'ua-parser-js';
 
 const TodoList: React.FC = () => {
+  const parser = new UAParser(navigator.userAgent);
+  const { type } = parser.getDevice();
+
   const theme = useContext(ThemeContext);
+
   return (
     <div className={`App ${theme?.darkMode ? 'darkmode' : 'lightmode'}`}>
       <Header />
       <hr />
-      <AlertMessage />
+      {type === 'mobile' ? <></> : <AlertMessage />}
       <Main />
       <Footer />
     </div>

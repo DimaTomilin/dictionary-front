543601c4574ff25fdcb356f55bb0a5ac14aa1002
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2022-08-08 16:46:51 +0300

    Adding offcanvas navbar
    New offcanvas menu that helping to go home page or history page
    
    All history word elements also have click event handler that will help to fast search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import DarkMode from './ui/DarkMode';
+import SideNavbar from './ui/HeaderNavbar';
 
 const Header: React.FC = () => {
   return (
     <div className='header'>
       <div className='up-part'>
-        <button>History</button>
+        <SideNavbar />
         <DarkMode />
       </div>
       <h1>Vocabulary</h1>
---
+++ src/components/ui/HeaderNavbar.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { AiOutlineMenu } from 'react-icons/ai';
+
+const SideNavbar: React.FC = () => {
+  const [show, setShow] = useState(false);
+  const navigate = useNavigate();
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div>
+      <Button className='sidebar' variant='warning' onClick={handleShow}>
+        <AiOutlineMenu />
+      </Button>
+
+      <Offcanvas show={show} onHide={handleClose} className='sidebar-field'>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Dictionary Menu</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <p
+            className='navbar-option'
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            Home
+          </p>
+          <p
+            className='navbar-option'
+            onClick={() => {
+              navigate('/history');
+            }}
+          >
+            History
+          </p>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </div>
+  );
+};
+
+export default SideNavbar;
